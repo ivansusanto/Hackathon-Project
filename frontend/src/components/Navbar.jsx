e2ce130f1d78d27b6709c2453baa1966a0256c37
Dionisius Mikha <Vitoutama82@gmail.com>
2023-09-23 02:28:12 +0700

    repisi navbar
    ngeganti item

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,21 +5,20 @@ function Navbar (){
   return(
     <>
     <div className="navbar">
-          <div className="navbar-wrapper">
-            <div className="overlap-group-wrapper">
-              <div className="overlap-group">
-                <div className="text-wrapper">Beranda</div>
-                <div className="div">Tour</div>
-                <div className="text-wrapper-2">Festival</div>
-                <div className="text-wrapper-3">UMKM</div>
-                <img className="hati-kanan" alt="Hati kanan" src="/img/Logo-lebar.png" />
-                <div className="overlap">
-                  <div className="text-wrapper-4">Sign in</div>
-                </div>
-              </div>
+      <div className="navbar-wrapper">
+        <div className="overlap-group-wrapper">
+          <div className="overlap-group">
+            <div className="text-wrapper">Beranda</div>
+            <div className="div">Acara</div>
+            <div className="text-wrapper-2">Wisata</div>
+            <img className="hati-kanan" alt="Hati kanan" src="/img/Logo-lebar.png" />
+            <div className="overlap">
+              <div className="text-wrapper-3">Sign in</div>
             </div>
           </div>
         </div>
+      </div>
+    </div>
     </>
   )
 }
