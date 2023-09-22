c9bbd0381f4c9f58a5e44e66d0a9191ff63a3ea6
AlexandroFebrian <105537730+AlexandroFebrian@users.noreply.github.com>
2023-09-23 03:54:30 +0700

    update navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,6 +4,7 @@ function Navbar (){
 
   return(
     <>
+<<<<<<< Updated upstream
     <div className="navbar">
           <div className="navbar-wrapper">
             <div className="overlap-group-wrapper">
@@ -22,7 +23,29 @@ function Navbar (){
                     </div>
                 </a>
               </div>
+=======
+    <div className="navbar ">
+      <div className="navbar-wrapper overflow-y-hidden ">
+        <div className="overlap-group-wrapper">
+          <div className="overlap-group">
+            <a href="/">
+              <img className="hati-kanan" alt="Hati kanan" src="/img/Logo-lebar.png" />
+            </a>
+            <div className="text-wrapper">
+              <a href="/">Beranda</a>
+>>>>>>> Stashed changes
             </div>
+            <div className="div">
+              <a href="/acara">Acara</a>
+            </div>
+            <div className="text-wrapper-2">
+              <a href="/wisata">Wisata</a>
+            </div>
+            <a href="/login">
+              <div className="overlap btn btn-accent">
+                <div className="text-wrapper-3">Login</div>
+              </div>
+            </a>
           </div>
         </div>
     </>
