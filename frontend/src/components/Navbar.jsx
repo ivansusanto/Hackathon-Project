c927ade8b86edcadba42f1e1735e5763e49181e2
Ivan Susanto <105846193+ivansusanto@users.noreply.github.com>
2023-09-23 03:09:45 +0700

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,13 +8,19 @@ function Navbar (){
           <div className="navbar-wrapper">
             <div className="overlap-group-wrapper">
               <div className="overlap-group">
-                <div className="div">Beranda</div>
+                <a href="/">
+                    <div className="div">Beranda</div>
+                </a>
                 <div className="text-wrapper-2">Wisata</div>
                 <div className="text-wrapper-3">Acara</div>
-                <img className="hati-kanan" alt="Hati kanan" src="/img/Logo-lebar.png" />
-                <div className="overlap">
-                  <div className="text-wrapper-4">Sign in</div>
-                </div>
+                <a href="/">
+                    <img className="hati-kanan" alt="Hati kanan" src="/img/Logo-lebar.png" />
+                </a>
+                <a href="/login">
+                    <div className="overlap">
+                        <div className="text-wrapper-4">Sign in</div>
+                    </div>
+                </a>
               </div>
             </div>
           </div>
