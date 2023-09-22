9346f624d63b4b94daca2ae035b4205ec42eed15
AlexandroFebrian <105537730+AlexandroFebrian@users.noreply.github.com>
2023-09-23 02:50:36 +0700

    add login & register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,16 @@ import { Routes, Route } from 'react-router-dom';
 import User from './pages/User';
 import NotFound from './pages/NotFound';
 import LandingPage from './pages/LandingPage/LandingPage';
+import Login from './pages/LoginPage/Login';
+import Register from './pages/RegisterPage/Register';
 
 const App = () => {
     return (
         <>
             <Routes>
                 <Route path="/" element={<LandingPage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </>
---
+++ frontend/src/pages/RegisterPage/Register.jsx
@@ -0,0 +1,168 @@
+import { useState, useEffect } from "react";
+import { useToast } from "@chakra-ui/react";
+import background from "/img/image 3.png"
+import Navbar from "../../components/Navbar";
+import TextField from '@mui/material/TextField';
+import passwordIcon from "/img/mdi-password.svg"
+import userIcon from "/img/mdi-user-outline.svg"
+import fetch from "../fetch";
+
+function Login (){
+  const [email, setEmail] = useState()
+  const [username, setUsername] = useState()
+  const [phone, setPhone] = useState()
+  const [password, setPassword] = useState()
+  const [confirmPassword, setConfirmPassword] = useState()
+  const [name, setName] = useState()
+  const [filled, setFilled] = useState(false)
+  const {http} = fetch()
+
+  function emailHandler(e){
+    setEmail(e.target.value)
+  }
+  
+  function usernameHandler(e){
+    setUsername(e.target.value)
+  }
+
+  function phoneHandler(e){
+    setPhone(e.target.value)
+  }
+
+  function passwordHandler(e){
+    setPassword(e.target.value)
+  }
+
+  function confirmPasswordHandler(e){
+    setConfirmPassword(e.target.value)
+  }
+  
+  function nameHandler(e){
+    setName(e.target.value)
+  }
+
+
+  useEffect(()=>{
+    checkFilled()
+  }, [email,phone, password, confirmPassword, name])
+
+  function checkFilled(){
+    if(!email || !username || !phone || !password || !confirmPassword || !name){
+      setFilled(false)
+    }else{
+      setFilled(true)
+    }
+    console.log(filled)
+  }
+  
+  function submitHandler(e){
+    if(!email || !username || !phone || !password || !confirmPassword || !name){
+      alert("Ada field yang belum diisi")
+    }else{
+      const data = {
+        email: email,
+        username: username,
+        password: password,
+        conf_pass: confirmPassword,
+        no_telp: phone,
+        display_name: name,
+        role: 1
+      }
+      http.post("users/register", data).then((res) => {
+        console.log(res)
+      })
+    }
+  }
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <div className=" h-screen overflow-hidden relative">
+    <img src={background} alt="" className="absolute z-0" />
+        <div className="relative flex z-10 justify-center">
+          <div className=" flex items-center justify-center h-screen">
+            <div className="border-solid border-2 rounded-lg py-12 px-28 bg-white" style={{height: "600px", width: "1000px"}}>
+              <h1 className=" text-7xl font-bold flex justify-center">REGISTER</h1>
+              <div className="mt-20 flex justify-between">
+                <div className="flex">
+                  <TextField 
+                  id="outlined-basic1" 
+                  label="Email" 
+                  variant="outlined" 
+                  sx={{width: "320px"}}
+                  onChange={emailHandler}
+                  />
+
+                </div>
+                <div className="flex">  
+                  <TextField 
+                  id="outlined-basic2" 
+                  label="Username" 
+                  variant="outlined" 
+                  sx={{width: "320px"}}
+                  onChange={usernameHandler}
+
+                  />
+
+                </div>
+              </div>
+              <div className="mt-5 flex justify-between">
+                <div className="flex">
+                  <TextField 
+                  id="outlined-password-input1" 
+                  label="Password" 
+                  variant="outlined" 
+                  sx={{width: "320px"}}
+                  onChange={passwordHandler}
+                  />
+
+                </div>
+                <div className="flex">  
+                  <TextField 
+                  id="outlined-password-input2" 
+                  label="Confirm Password" 
+                  variant="outlined" 
+                  sx={{width: "320px"}}
+                  onChange={confirmPasswordHandler}
+
+                  />
+
+                </div>
+              </div>
+              <div className="mt-5 flex justify-between">
+                <div className="flex">
+                  <TextField 
+                  id="outlined-basic2" 
+                  label="Nomor telepon" 
+                  variant="outlined" 
+                  sx={{width: "320px"}}
+                  onChange={phoneHandler}
+
+                  />
+                  
+                </div>
+                <div className="flex">
+                  <TextField 
+                  id="outlined-basic5" 
+                  label="Nama Pengguna" 
+                  variant="outlined" 
+                  sx={{width: "320px"}}
+                  onChange={nameHandler}
+                  />
+
+                </div>
+              </div>
+              
+              <div>
+                <button className="btn btn-primary w-full mt-10 mb-1 text-xl font-bold" onClick={submitHandler} disabled={!filled}>Masuk</button>
+              </div>
+              Sudah pernah mendaftar? Silahkan <a href="/login" className=" text-primary"><u>masuk</u></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login
