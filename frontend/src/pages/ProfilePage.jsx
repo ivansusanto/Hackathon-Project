e122c23c19573ecb721cfecd1ce453fa293b812f
AlexandroFebrian <105537730+AlexandroFebrian@users.noreply.github.com>
2023-09-24 01:12:08 +0700

    Update ProfilePage.jsx

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,36 +1,94 @@
 import { TextField } from "@mui/material";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import { useEffect, useState } from "react";
+import fetch from "./fetch";
+import { Input } from "@chakra-ui/input";
 
 
 export default function ProfilePage(){
+  if(!sessionStorage.getItem("token")) window.location.replace("/")
+
+  const {http} = fetch()
+  const [user, setUser] = useState({})
+  const [name, setName] = useState()
+  const [phone, setPhone] = useState()
+
+  useEffect(() => {
+    http.get("users", {
+      headers:{
+        'Accept': 'application/json',
+        Authorization: 'Bearer ' + sessionStorage.getItem("token")
+      }
+    }).then((res) => {
+      setUser(res.data.user)
+      setName(res.data.user.display_name)
+      setPhone(res.data.user.no_telp)
+    })
+  }, [])
   
+  function nameHandler(e){
+    console.log(e.target.value)
+    setName(e.target.value)
+  }
+
+  function phoneHandler(e){
+    setPhone(e.target.value)
+  }
+
+  function simpanHandler(){
+    let data = {
+      display_name: name,
+      no_telp: phone
+    }
+    console.log(data)
+    http.put("users/update", { data }, {
+      headers:{
+        "Content-type":"multipart/form-data",
+        Authorization: 'Bearer ' + sessionStorage.getItem("token")
+      }
+    }).then((res) => {
+     console.log(res)
+    })
+  }
 
   return (
     <>
       <Navbar />
-      <div className="w-full relative h-[900px] flex justify-center items-center">
-        <div className="w-2/5 h-4/5 -mt-16">
+      <div className="w-full relative h-[704px] flex justify-center items-center">
+        <div className="w-2/5 h-2/3 -mt-16">
           <h1 className="font-bold text-5xl mb-6 ms-6 text">Edit profile</h1>
           <div className="w-full h-full border shadow-2xl rounded-3xl px-16 pt-12">
             
             <div className="w-full flex">
               <div className="w-1/2 pe-5">
-                <TextField id="outlined-basic1" label="Email" variant="outlined" className="w-full"/>
+                Email
+              < Input placeholder='Email' className="border rounded-md p-3 w-full" disabled={true} value={user.email || ""}/>
+                
               </div>
-              <div className="w-1/2 flex justify-end ps-5">
-                <TextField id="outlined-basic2" label="Username" variant="outlined" className="w-full" />
+              <div className="w-1/2 ps-5">
+                <div>
+                  Username
+                </div>
+                <div>
+                  <Input placeholder='Email' className="border rounded-md p-3 w-full" disabled={true} value={user.email || ""}/>
+
+                </div>
               </div>
             </div>
 
             <div className="mt-7">
-              <TextField id="outlined-basic2" label="Nama Pengguna" variant="outlined" className="w-full" />
+              Nama pengguna
+            <Input placeholder='Nama pengguna' className="border rounded-md p-3 w-full" defaultValue={user.display_name} onChange={nameHandler}/>
 
             </div>
             <div className="mt-7">
-              <TextField id="outlined-basic2" label="Nomor telepon" variant="outlined" className="w-full" />
+              Nomor Telepon
+            <Input placeholder='Nomor telepon' className="border rounded-md p-3 w-full" defaultValue={user.no_telp} onChange={phoneHandler}/>
 
             </div>
+
+            <button className="btn btn-neutral w-full mt-7" onClick={simpanHandler}>Simpan</button>
 
             
 
