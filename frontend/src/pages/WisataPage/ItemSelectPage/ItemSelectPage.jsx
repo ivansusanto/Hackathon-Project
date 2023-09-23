e2deba62324b981bcd0f3265194a5b3a299934a8
Dionisius Mikha <Vitoutama82@gmail.com>
2023-09-24 01:34:55 +0700

    update item selected wisata

--- frontend/src/pages/WisataPage/ItemSelectPage/ItemSelectPage.jsx
+++ frontend/src/pages/WisataPage/ItemSelectPage/ItemSelectPage.jsx
@@ -15,35 +15,37 @@ export const ItemSelectPage = () => {
       setWisata(res.data.data)
     })
   }, [])
+
+  const [amount, setAmount] = useState(1);
   
 
   return (
-    <div className="bg-white flex flex-row justify-center w-full">
-      <div className="bg-white w-[1920px] h-[954px] relative">
-
-        {/* Item yang dipilih */}
-        <img className="absolute w-[400px] h-[400px] top-[95px] left-[144px] object-cover" alt="Item" src={wisata.foto} />
-
-        <div className="absolute w-[672px] top-[94px] left-[708px] [font-family:'Montserrat-Bold',Helvetica] font-bold text-black text-[40px] tracking-[0] leading-[normal]">
-          {wisata.name}
+    <div className='flex flex-col justify-between lg:flex-row gap-16 lg:items-center'>
+            <div className='flex flex-col gap-6 lg:w-2/4'>
+                <img src={wisata.foto} alt="" className='w-full h-full aspect-square object-cover rounded-xl'/>
+                <div className='flex flex-row justify-between h-24'>
+                    <img src={wisata.foto} alt="" className='w-24 h-24 rounded-md cursor-pointer' onClick={() => setActiveImage(wisata.foto)}/>
+                </div>
+            </div>
+            {/* ABOUT */}
+            <div className='flex flex-col gap-4 lg:w-2/4'>
+                <div>
+                    <h1 className='text-3xl font-bold'>{wisata.name}</h1>
+                </div>
+                <p className='text-gray-700'>
+                  {wisata.desc}
+                </p>
+                <h6 className='text-2xl font-semibold'>{wisata.harga}</h6>
+                <div className='flex flex-row items-center gap-12'>
+                    <div className='flex flex-row items-center'>
+                        <button className='bg-gray-200 py-2 px-5 rounded-lg text-violet-800 text-3xl' onClick={() => setAmount((prev) => prev - 1)}>-</button>
+                        <span className='py-4 px-6 rounded-lg'>{amount}</span>
+                        <button className='bg-gray-200 py-2 px-4 rounded-lg text-violet-800 text-3xl' onClick={() => setAmount((prev) => prev + 1)}>+</button>
+                    </div>
+                    <button className='bg-violet-800 text-white font-semibold py-3 px-16 rounded-xl h-full'>Add to Cart</button>
+                </div>
+            </div>
         </div>
-        <div className="absolute w-[668px] top-[195px] left-[712px] [font-family:'Montserrat-Light',Helvetica] font-light text-black text-[40px] tracking-[0] leading-[normal]">
-          Deskripsi : <br></br> 
-          {wisata.desc}
-        </div>
-
-        {/* Button */}
-        <img
-          className="absolute w-[321px] h-[93px] top-[73px] left-[1489px] object-cover"
-          alt="Button booking"
-          src="button-booking.png"
-        />
-
-        {/* MAP */}
-        <img className="absolute w-[1236px] h-[292px] top-[596px] left-[144px] object-cover" alt="Map" src="map.png" />
-
-        
-      </div>
-    </div>
+    
   );
 };
